a86b273b882fb4757df58844f860bf03034379ff
Riley Kuhlman <rileyk2@icloud.com>
2023-11-27 18:27:14 -0600

    Finish citations.

--- src/Notes.tsx
+++ src/Notes.tsx
@@ -7,9 +7,13 @@ const takeExtinctionOffYourPlate = <>
         The source states that Americans waste 1200 calories per day. Multiplying by 365 days, this would indicate that 438,000 calories are wasted in a year.
     </>;
 
+const emissionsOfDiets = <>
+Nabipour Afrouzi, H., et al. “A Comprehensive Review on Carbon Footprint of Regular Diet and Ways to Improving Lowered Emissions.” <i>Results in Engineering</i>, vol. 18, 1 June 2023, p. 101054, <a href="http://www.sciencedirect.com/science/article/pii/S2590123023001810">www.sciencedirect.com/science/article/pii/S2590123023001810</a>.
+</>
+
 const notes = [
     <>
-        “ReFED - Food Waste Monitor.” <i>Insights Engine</i>, <a href="insights-engine.refed.org/food-waste-monitor?break_by=destination&indicator=tons-waste&view=detail&year=2021">insights-engine.refed.org/food-waste-monitor?break_by=destination&indicator=tons-waste&view=detail&year=2021</a>. Accessed 27 Nov. 2023.
+        “ReFED - Food Waste Monitor.” <i>Insights Engine</i>, <a href="http://insights-engine.refed.org/food-waste-monitor?break_by=destination&indicator=tons-waste&view=detail&year=2021">insights-engine.refed.org/food-waste-monitor?break_by=destination&indicator=tons-waste&view=detail&year=2021</a>. Accessed 27 Nov. 2023.
     </>,
     <>
         “Food Waste in America | Feeding America.” <i>Feeding America</i>, <a href="http://www.feedingamerica.org/our-work/reduce-food-waste">www.feedingamerica.org/our-work/reduce-food-waste</a>. Accessed 27 Nov. 2023.
@@ -87,7 +91,39 @@ const notes = [
     According to the USDA, 100g of steak is equivalent to 26g of protein. According to the source below, the average amount of meat obtained from a cow is 480 pounds. That comes out to 54431 grams.
     <br />
     “Amount of Freezer Beef Expected from a Carcass?” <i>Beef2Live</i>, <a href="http://beef2live.com/story-amount-freezer-beef-expected-carcass-0-111639">beef2live.com/story-amount-freezer-beef-expected-carcass-0-111639</a>. Accessed 27 Nov. 2023.
-    </>
+    </>,
+
+    <>
+    James Videle. “Comparison of Protein and Caloric Energy (KCal) Produced per Acre on U.S. Farms.” <i>The Humane Herald</i>, 9 May 2019, <a href="humaneherald.org/2019/05/09/comparison-of-protein-and-caloric-energy-kcal-produced-per-acre-on-u-s-farms/">humaneherald.org/2019/05/09/comparison-of-protein-and-caloric-energy-kcal-produced-per-acre-on-u-s-farms/</a>. Accessed 27 Nov. 2023.
+    </>,
+
+    emissionsOfDiets,
+
+    <>
+    “Feeding Beef Cattle.” <i>Penn State Extension</i>, <a href="http://extension.psu.edu/feeding-beef-cattle">extension.psu.edu/feeding-beef-cattle</a>. Accessed 27 Nov. 2023.
+    </>,
+
+    <>
+    “Chicken Production Volume U.S. 2009-2019.” <i>Statista</i>, <a href="http://www.statista.com/statistics/1108994/us-total-chicken-production/">www.statista.com/statistics/1108994/us-total-chicken-production/</a>. Accessed 27 Nov. 2023.
+    </>,
+
+    <>
+    Larsen, Janet. “Peak Meat: U.S. Meat Consumption Falling.” Earth-Policy.org, <i>Earth Policy Institute</i>, 7 Mar. 2012, <a href="http://www.earth-policy.org/data_highlights/2012/highlights25">www.earth-policy.org/data_highlights/2012/highlights25</a>. Accessed 27 Nov. 2023.
+    </>,
+
+    emissionsOfDiets,
+
+    <>
+    Molidor, Jennifer. “Checked out - Medium.” <i>Medium</i>, 10 Apr. 2018, <a href="medium.com/checked-out">medium.com/checked-out</a>. Accessed 28 Nov. 2023.
+    </>,
+
+    <>
+    Lallanilla, Mark. “50% of US Beef Is Eaten by Just 12% of Americans, Mostly Men.” <i>NY Post</i>, 31 Aug. 2023, <a href="http://nypost.com/2023/08/31/50-of-us-beef-is-eaten-by-just-12-of-americans-mostly-men-study/">nypost.com/2023/08/31/50-of-us-beef-is-eaten-by-just-12-of-americans-mostly-men-study/</a>.
+    </>,
+
+    <>
+    “ReFED - Food Waste Monitor.” <i>Insights Engine</i>, <a href="https://insights-engine.refed.org/food-waste-monitor?break_by=cause&indicator=tons-surplus&sector=residential&view=detail&year=2021">insights-engine.refed.org/food-waste-monitor?break_by=cause&indicator=tons-surplus&sector=residential&view=detail&year=2021</a>. Accessed 27 Nov. 2023.
+    </>,
 ]
 
 export default function Notes() {
